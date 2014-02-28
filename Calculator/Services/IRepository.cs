9083d1aa3f9f6315268edbc3a86297cbe40af153
Igor Chelyadinskiy <ichelyadinskiy@gmail.com>
2014-02-28 03:43:13 +0200

    Last commit

---
+++ Calculator/Services/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Services
+{
+    public interface IRepository
+    {
+        object AddRes(Model model);
+        object ReplaceMemory(Model model);
+        object GetMemory();
+        object AddToMemory(Model model);
+        object MinusRes(Model model);
+        object DivisionRes(Model model);
+        object MultiRes(Model model);
+        object Clear();
+    }
+}
