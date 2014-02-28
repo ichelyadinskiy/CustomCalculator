948f5f39dc9d0263426cab305d76ac1d9528ff04
ichelyadinskiy <ichelyadinskiy@gmail.com>
2014-02-28 23:04:06 +0300

    Final version (but without knockout.js)

---
+++ Calculator.Tests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.Controllers;
+using Calculator.Models;
+using Moq;
+
+namespace Calculator.Tests.Controllers
+{
+    [TestClass]
+    public class ValuesControllerTest
+    {
+        [TestMethod]
+        public void SendSumTest()
+        {
+            var controller = new ValuesController();
+            Model modelTest = new Model { Current = 10, Memory = 0, Result = 0 };
+            modelTest = (Model)controller.PostSum(modelTest);
+            Assert.AreEqual(10, modelTest.Result);
+        }
+
+        [TestMethod]
+        public void GetMemoryTest()
+        {
+            var controller = new ValuesController();
+            Assert.IsNotNull(controller.GetMemory());
+        }
+
+        [TestMethod]
+        public void Clear()
+        {
+            var controller = new ValuesController();
+            Model model = (Model)controller.GetClearAll();
+            var result = model.Result + model.Memory + model.Current;
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ Calculator.Tests/Services/ServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Calculator.Models;
+using Calculator.Resolvers.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests.Services
+{
+    [TestClass]
+    public class ServiceTests
+    {
+        [TestMethod]
+        public void Multiplication()
+        {
+            Model model = new Model { Result = 3, Current = 3, Memory = 0};
+            IService service = Mock.Of<IService>(x => x.MultiRes(model) == new Model { Result = 9, Current = 0, Memory = 0});
+            var mult = service.MultiRes(model);
+            Assert.AreEqual(9, mult.Result);
+        }
+    }
+}
