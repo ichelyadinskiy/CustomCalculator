cfc83de99776c20db8c28537e29d39c8b8336f6c
ichelyadinskiy <ichelyadinskiy@gmail.com>
2014-02-28 23:07:50 +0300

    To publish

---
+++ Calculator.Tests/Repositories/RepositoryTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.Resolvers.Repositories;
+using Calculator.Models;
+using Moq;
+
+namespace Calculator.Tests.Repositories
+{
+    [TestClass]
+    public class RepositoryTests
+    {
+        [TestMethod]
+        public void objCreate()
+        {
+            Model model = new Model();
+            Assert.IsNotNull(model);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void AddResult()
+        {
+            var repository = new Repository();
+            repository.AddRes(null);
+        }
+
+        [TestMethod]
+        public void AddMemory()
+        {
+            var repository = new Repository();
+            Model model = repository.GetMemory();
+            Assert.IsNotNull(model);
+        }
+
+        [TestMethod]
+        public void Division()
+        {
+            var model = new Model { Current = 10, Result = 40 };
+            IRepository repDep = Mock.Of<IRepository>(x => x.DivisionRes(model) == new Model { Result = 4, Current = 0 });
+            var div = repDep.DivisionRes(model);
+            Assert.AreEqual(4, div.Result);
+        }
+    }
+}
